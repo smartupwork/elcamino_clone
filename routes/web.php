d71aa3323bea3dd1cc83f56f0c6db330fa4b881b
Obaid <smartupwork80@gmail.com>
2024-03-21 15:36:38 -0400

    clone changes

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::get('/',[App\Http\Controllers\Auth\LoginController::class,'login']);
 Route::group(['middleware' => 'auth'], function (){
-    Route::redirect('/dashboard', '/calculations')->name('dashboard');
     Route::resource('calculations', 'CalculationController');
-    Route::post('calculator/ajax-save', 'CalculationController@ajax_save');
-    Route::resource('users', 'UserController');
-
-    Route::get('profile/edit', 'UserController@profile')->name('profile.edit');
-
-    //snapshare
-    Route::get('/impersonate/{id}',[App\Http\Controllers\UserController::class,'imperson'])->name('/impersonate/{id}');
-    Route::get('copy/{id}',[App\Http\Controllers\CalculationController::class,'copy'])->name('copy/{id}');
-    Route::get('update/{id}',[App\Http\Controllers\CalculationController::class,'update_snapshot'])->name('update/{id}');
 
 });
 
